bb75530ae5a050edb94b9c55f4826bd693fe97b8
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-09-05 15:03:17 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,20 @@
 # Implement your procedural solution here!
+require 'pry'
+def even_fibonacci_sum(limit)
+    sum = 0
+    num = 1
+    prev_num = 1
+    while prev_num+num <= limit
+        #calculate the next sum of sequence
+        new_num = prev_num+num
+        prev_num = num
+        num = new_num
+        if new_num % 2 == 0
+            sum += new_num
+        end
+    end
+    return sum
+end
+
+
+#even_fibonacci_sum(100)
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,25 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+    attr_accessor :limit, :sums
+    def initialize(limit)
+        @limit = limit
+        @sums = 0
+    end
+
+    def sum
+        num = 1
+        prev_num = 1
+        while prev_num+num <= self.limit
+            #calculate the next sum of sequence
+            new_num = prev_num+num
+            prev_num = num
+            num = new_num
+            if new_num % 2 == 0
+                self.sums += new_num
+            end
+        end
+        return self.sums
+    end
+end
+
+    
